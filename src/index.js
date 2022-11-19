dfa1d6f4b6330edd690e8cda518189cd5397f5aa
Muhammad Mahmood <blackbird2013.44@gmail.com>
2022-11-19 12:40:48 +0200

    Add full page background image

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './fonts/inter-variablefont_slnt,wght-subset.zopfli.woff';
 import './sass/styles.scss';
 import './js/script.js';
 
-import './media/cool.jpg';
+// import './media/bg.jpg';
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,1 @@
-document.querySelector('h1').textContent = 'Hello, World!';
+console.log('Hello, World!');
